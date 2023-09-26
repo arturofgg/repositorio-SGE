f67f4b4b3a3d98d18353b04f28d2a0ebfeb4a1ba
arturofgg <arturofdezgg17@gmail.com>
2023-09-26 10:00:39 +0200

    e3

---
+++ Unidad1/ejercicio3/e3.js
@@ -0,0 +1,14 @@
+//alerta
+alert("ALERTA");
+
+//consola
+console.log("LOGS");
+console.info("info");
+console.warn("advertencia");
+console.error("error");
+
+//declaracion variables y concatenar
+let num1=1, num2=3, suma=num1+num2;
+if(true)
+false
+console.log("La suma de "+num1+" + "+num2+" = "+suma);
