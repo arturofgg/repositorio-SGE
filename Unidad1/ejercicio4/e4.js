ac133571030833740c723a3e6e80452fb5c6356b
arturofgg <arturofdezgg17@gmail.com>
2023-09-28 10:56:20 +0200

    e4

---
+++ Unidad1/ejercicio4/e4.js
@@ -0,0 +1,10 @@
+var ordenador = 700, cascos = 50, teclado = 70;
+let total = (ordenador+cascos+teclado)*0.9;
+document.write(`El total que te vas a gastar es: ${total}`);
+
+cascos = parseInt(prompt("Cuanto cuestan los cascos"));
+raton = parseInt(prompt("Cuanto cuesta el raton"));
+
+let total2= cascos+raton;
+document.write("<br> El total2 que te vas a gastar es "+ total2);
+document.write
